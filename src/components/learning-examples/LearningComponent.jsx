633741f01362d608164161f8ef8349a36ebc9a34
saikrishnakovuru <saikrishnakovuru1995@gmail.com>
2023-02-22 18:24:08 -0500

    Learning component

---
+++ src/components/learning-examples/LearningComponent.jsx
@@ -0,0 +1,23 @@
+// import FirstComponent from "./FirstComponent";
+// import SecondComponent from "./SecondComponent";
+// import LearningJavaScript from "./LearningJavaScript";
+
+// export function LearningComponent(){
+//   return (
+//     <>
+//       <FirstComponent/>
+//       <SecondComponent/>
+//       <LearningJavaScript/>
+//     </>
+//   );
+// }
+
+// export const LearningComponents = () => {
+//   return (
+//     <>
+//       <FirstComponent/>
+//       <SecondComponent/>
+//       <LearningJavaScript/>
+//     </>
+//   );
+// }
